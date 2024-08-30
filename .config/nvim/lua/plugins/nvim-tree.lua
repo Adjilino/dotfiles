5302db47fa5963e24b3fe155d9d72020a699661a
Adjilino <adjilino@gmail.com>
2024-08-30 19:42:13 +0100

    lint and find file for nvim tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -12,74 +12,80 @@ local WIDTH_RATIO = 0.5   -- You can change this too
 local keymap = vim.keymap -- for conciseness
 
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "kyazdani42/nvim-web-devicons" },
-    config = function()
-        local tree = require("nvim-tree")
-        local api = require("nvim-tree.api")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "kyazdani42/nvim-web-devicons" },
+	config = function()
+		local tree = require("nvim-tree")
+		local api = require("nvim-tree.api")
 
-        local function my_on_attach(bufnr)
-            local function opts(desc)
-                return {
-                    desc = "nvim-tree: " .. desc,
-                    buffer = bufnr,
-                    noremap = true,
-                    silent = true,
-                    nowait = true
-                }
-            end
+		local function my_on_attach(bufnr)
+			local function opts(desc)
+				return {
+					desc = "nvim-tree: " .. desc,
+					buffer = bufnr,
+					noremap = true,
+					silent = true,
+					nowait = true
+				}
+			end
 
-            -- default mappings
-            api.config.mappings.default_on_attach(bufnr)
-        end
+			-- default mappings
+			api.config.mappings.default_on_attach(bufnr)
+		end
 
-        tree.setup({
-            disable_netrw = true,
-            hijack_netrw = true,
-            respect_buf_cwd = true,
-            sync_root_with_cwd = true,
-            view = {
-                relativenumber = true,
-                float = {
-                    enable = true,
-                    open_win_config = function()
-                        local screen_w = vim.opt.columns:get()
-                        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-                        local window_w = screen_w * WIDTH_RATIO
-                        local window_h = screen_h * HEIGHT_RATIO
-                        local window_w_int = math.floor(window_w)
-                        local window_h_int = math.floor(window_h)
-                        local center_x = (screen_w - window_w) / 2
-                        local center_y = ((vim.opt.lines:get() - window_h) / 2)
-                            - vim.opt.cmdheight:get()
-                        return {
-                            border = "rounded",
-                            relative = "editor",
-                            row = center_y,
-                            col = center_x,
-                            width = window_w_int,
-                            height = window_h_int,
-                        }
-                    end
-                },
-                width = function()
-                    return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
-                end,
-            },
-            on_attach = my_on_attach,
-            -- filters = {
-            --   custom = { "^.git$" },
-            -- },
-            -- renderer = {
-            --   indent_width = 1,
-            -- },
-        })
+		tree.setup({
+			disable_netrw = true,
+			hijack_netrw = true,
+			respect_buf_cwd = true,
+			sync_root_with_cwd = true,
+			view = {
+				relativenumber = true,
+				float = {
+					enable = true,
+					open_win_config = function()
+						local screen_w = vim.opt.columns:get()
+						local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+						local window_w = screen_w * WIDTH_RATIO
+						local window_h = screen_h * HEIGHT_RATIO
+						local window_w_int = math.floor(window_w)
+						local window_h_int = math.floor(window_h)
+						local center_x = (screen_w - window_w) / 2
+						local center_y = ((vim.opt.lines:get() - window_h) / 2)
+							- vim.opt.cmdheight:get()
+						return {
+							border = "rounded",
+							relative = "editor",
+							row = center_y,
+							col = center_x,
+							width = window_w_int,
+							height = window_h_int,
+						}
+					end
+				},
+				width = function()
+					return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+				end,
+			},
+			on_attach = my_on_attach,
+			-- filters = {
+			--   custom = { "^.git$" },
+			-- },
+			-- renderer = {
+			--   indent_width = 1,
+			-- },
+		})
 
-        keymap.set("n", "<leader>tt", api.tree.toggle, {
-            desc = "[T]oggle [T]ree file explorer",
-            noremap = true,
-            silent = true,
-            nowait = true
-        }) -- toggle file explorer
-    end,
+		local open = function()
+			api.tree.toggle({
+				find_file = true
+			})
+		end
+
+		keymap.set("n", "<leader>tt", open, {
+			desc = "[T]oggle [T]ree file explorer",
+			noremap = true,
+			silent = true,
+			nowait = true
+		}) -- toggle file explorer
+	end,
 }
